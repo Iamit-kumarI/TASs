4b2da88c81a1660aa8ef0b6d9a1a28e64b2f9c30
Amit <amitkumaramitkumar15351@gmail.com>
2026-02-09 22:58:51 +0530

    created repo just

---
+++ src/main/java/com/Tas/TAS/TaSsystemApplication.java
@@ -0,0 +1,13 @@
+package com.Tas.TAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaSsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaSsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tas/TAS/TaSsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tas.TAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaSsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
